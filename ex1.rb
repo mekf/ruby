eccd13900914ff7cf8b2e1cd5b20492bc63c8f43
Minh Ha <nhat.minh.ha@gmail.com>
2013-02-25 18:51:40 +1300

    finished ex1

---
+++ ex1.rb
@@ -0,0 +1,16 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts 'Yay! Printing.'
+puts "I'd much rather you 'not'."
+puts 'I "said" do no touch this.'
+puts ""
+
+puts "#Extra Credit"
+puts "1. Make your script print another line."
+puts "Another the line"
+
+puts "2. Make your script print only one of the lines."
+lines = ["line1", "line2", "line3", "line4", "line5", "line6"]
+puts lines.sample(1)
