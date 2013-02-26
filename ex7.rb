f0409a1ec490c3a81fe786bbb2f6797d8a70df19
Minh Ha <nhat.minh.ha@gmail.com>
2013-02-26 22:18:50 +1300

    finished ex7

---
+++ ex7.rb
@@ -0,0 +1,31 @@
+# Exercise 7: More Printing
+puts "Exercise 7: More Printing"
+
+# print these string + append a new line
+puts "Mary had a little lamb."
+puts "its fleece was a white as %s." % 'snow'
+puts "and everywhere that Mary went."
+puts "." * 10 # print this dot 10 times
+
+# assign to variable end# with a strings
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# print the concatenation of variables end#
+print end1 + end2 + end3 + end4 + end5 + end6
+print end7 + end8 + end9 + end10 + end11 + end12
+
+puts
+
+puts 'Extra Credit'
+puts '1. Commenting'
