2b4aa3578263794acf0a5b578fa3f3f9019a8bec
Minh Ha <nhat.minh.ha@gmail.com>
2013-03-12 00:39:44 +1300

    fixed typo

--- ex31_Making_Decisions.rb
+++ ex31_Making_Decisions.rb
@@ -1,19 +1,19 @@
 puts "Exercise 31: Making Decisions"
 
-def promt
+def prompt
   print "> "
 end
 
 puts "You entered a dark room with two doors. Do you go through door #1 or door #2?"
 
-promt; door = gets.chomp
+prompt; door = gets.chomp
 
 if door == "1"
   puts "There's a giant bear eating a cheese cake. What do you do?"
   puts "1. Take the take."
   puts "2. Scream at the bear."
 
-  promt; bear = gets.chomp
+  prompt; bear = gets.chomp
 
   if bear == "1"
     puts "The bear eats your face off. Good job."
@@ -29,12 +29,12 @@ elsif door == "2"
   puts "2. Yellow jacket clothespins."
   puts "3. Understanding revolvers yelling melodies."
 
-  promt; insanity = gets.chomp
+  prompt; insanity = gets.chomp
 
   if insanity == "1" or insanity == "2"
     puts "Your body survivies powered by a mind of jello. Good job!"
   else
-    puts "The isanity rots your eyes into a pool of muck. Good job!"
+    puts "The insanity rots your eyes into a pool of muck. Good job!"
   end
 
 else
