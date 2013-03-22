1c5fba8cbbca741087cccdfa2b8d2a397a5cc1b8
Minh Ha <nhat.minh.ha@gmail.com>
2013-03-22 22:25:16 +1300

    finished extra credit

--- ruby_the_hard_way/ex33_While_Loop.rb
+++ ruby_the_hard_way/ex33_While_Loop.rb
@@ -30,3 +30,26 @@ print "What's the numer for the increment: "
 increment = gets.chomp.to_i
 
 while_loop(number, increment)
+
+puts
+puts "3. Using for-loop n' range"
+
+num_for = []
+for j in (0..7)
+  puts "\n# at the top of for: #{j}"
+  num_for.push(j)
+
+  # j += 1
+
+  puts "# now: #{num_for}"
+  puts "# at the bottom of for: #{j}"
+end
+
+puts
+puts <<BLOCK
+If for-loop is used with a range then the increment is not neccessary.
+It will jump to the next value within the range automatically.
+There will be no difference between the value at the top and bottom.
+If we dont get rid of the increment then there will be the difference between the value
+at the top and bottom of the for-loop.
+BLOCK
