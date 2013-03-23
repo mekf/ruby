a3a3feab2d8f94f9fa29eb9a877942fb5d22e1a4
Minh Ha <nhat.minh.ha@gmail.com>
2013-03-23 22:46:04 +1300

    better looking hash

--- ruby_the_hard_way/ex41_Gorthons_From_Planet_Percal_#25.rb
+++ ruby_the_hard_way/ex41_Gorthons_From_Planet_Percal_#25.rb
@@ -17,28 +17,28 @@ def death
 end
 
 def central_corridor
-  puts @central_corridor_text["start"]
+  puts @central_corridor_text[:start]
   prompt; action = gets.chomp
 
   if action == "shoot!"
-    puts @central_corridor_text["shoot_die"]
+    puts @central_corridor_text[:shoot_die]
     return :death
   elsif action == "dodge!"
-    puts @central_corridor_text["dodge_die"]
+    puts @central_corridor_text[:dodge_die]
     return :death
   elsif action == "tell a joke"
-    puts @central_corridor_text["joke_go"]
+    puts @central_corridor_text[:joke_go]
     return :laser_weapon_armory
   else
-    puts @central_corridor_text["stuck"]
+    puts @central_corridor_text[:stuck]
     return :central_corridor
   end
 end
 
 def laser_weapon_armory
-  puts @laser_weapon_armory_text["keypad_start"]
+  puts @laser_weapon_armory_text[:keypad_start]
   code = "%s%s%s" % [rand(9) + 1, rand(9) + 1, rand(9) + 1]
-  print "cheat: #{code}"
+  puts "cheat: #{code}"
 
   print "[keypad]> "; guess = gets.chomp
   guesses = 0
@@ -50,43 +50,43 @@ def laser_weapon_armory
   end
 
   if guess == code
-    puts @laser_weapon_armory_text["correct_code"]
+    puts @laser_weapon_armory_text[:correct_code]
     return :the_bridge
   else
-    puts @laser_weapon_armory_text["time_out"]
+    puts @laser_weapon_armory_text[:time_out]
     return :death
   end
 end
 
 def the_bridge
-  puts @the_bridge_text["bridge_start"]
+  puts @the_bridge_text[:bridge_start]
   prompt; action = gets.chomp
   
   if action == "throw the bomb"
-    puts @the_bridge_text["throw_bomb_die"]
+    puts @the_bridge_text[:throw_bomb_die]
     return :death
   elsif action == "slowly place the bomb"
-    puts @the_bridge_text["place_bomb_die"]
+    puts @the_bridge_text[:place_bomb_die]
     return :escape_pod
   else
-    puts @the_bridge_text["stuck"]
+    puts @the_bridge_text[:stuck]
     return :the_bridge
   end
 end
 
 def escape_pod
-  puts @escape_pod_text["pod_start"]
+  puts @escape_pod_text[:pod_start]
   good_pod = rand(5) + 1
   puts "cheat: #{good_pod}"
   print "[pod]> "; guess = gets.chomp
 
   if guess.to_i != good_pod
     puts "You jump into pod %s and hit the eject button." % guess
-    puts @escape_pod_text["bad_pod_die"]
+    puts @escape_pod_text[:bad_pod_die]
     return :death
   else
     puts "You jump into pod %s and hit the eject button." % guess
-    puts @escape_pod_text["good_pod_win"]
+    puts @escape_pod_text[:good_pod_win]
     Process.exit(0)
   end
 end
--- ruby_the_hard_way/ex41_texts.rb
+++ ruby_the_hard_way/ex41_texts.rb
@@ -1,5 +1,5 @@
 @central_corridor_text = {
-  "start" => "The Gothons of Planet Percal #25 have invaded your ship and destroyed
+  start: "The Gothons of Planet Percal #25 have invaded your ship and destroyed
   your entire crew. You are the last surviving member and your last
   mission is to get the neutron destruct bomb from the Weapons Armory,
   put it in the bridge, and blow the ship up after getting into an escape pod.
@@ -8,32 +8,32 @@
   flowing around his hate filled body. He's blocking the door to the
   Armory and about to pull a weapon to blast you.",
   
-  "shoot_die" => "Quick on the draw you yank out your blaster and fire it at the Gothon.
+  shoot_die: "Quick on the draw you yank out your blaster and fire it at the Gothon.
   His clown costume is flowing and moving around his body, which throws
   off your aim. Your laser hits his costume but misses him entirely. This
   completely ruins his brand new costume his mother bought him, which
   makes him fly into an insane rage and blast you repeatedly in the face until
   you are dead. Then he eats you.",
   
-  "dodge_die" => "Like a world class boxer you dodge, weave, slip and slide right
+  dodge_die: "Like a world class boxer you dodge, weave, slip and slide right
   as the Gothon's blaster cranks a laser past your head.
   In the middle of your artful dodge your foot slips and you
   bang your head on the metal wall and pass out.
   You wake up shortly after only to die as the Gothon stomps on
   your head and eats you.",
 
-  "joke_go" => "Lucky for you they made you learn Gothon insults in the academy.
+  joke_go: "Lucky for you they made you learn Gothon insults in the academy.
   You tell the one Gothon joke you know:
   Lbhe zbgure vf fb sng, jura fur fvgf nebhaq gur ubhfr, fur fvgf nebhaq gur ubhfr.
   The Gothon stops, tries not to laugh, then busts out laughing and can't move.
   While he's laughing you run up and shoot him square in the head
   putting him down, then jump through the Weapon Armory door.",
   
-  "stuck" => "DOES NOT COMPUTE"
+  stuck: "DOES NOT COMPUTE"
 }
 
 @laser_weapon_armory_text = {
-  "keypad_start" => "You do a dive roll into the Weapon Armory, crouch and scan the room
+  keypad_start: "You do a dive roll into the Weapon Armory, crouch and scan the room
   for more Gothons that might be hiding. It's dead quiet, too quiet.
   You stand up and run to the far side of the room and find the
   neutron bomb in its container. There's a keypad lock on the box
@@ -41,32 +41,32 @@
   wrong 10 times then the lock closes forever and you can't
   get the bomb. The code is 3 digits.",
   
-  "correct_code" => "The container clicks open and the seal breaks, letting gas out.
+  correct_code: "The container clicks open and the seal breaks, letting gas out.
   You grab the neutron bomb and run as fast as you can to the
   bridge where you must place it in the right spot.",
   
-  "time_out" => "The lock buzzes one last time and then you hear a sickening
+  time_out: "The lock buzzes one last time and then you hear a sickening
   melting sound as the mechanism is fused together.
   You decide to sit there, and finally the Gothons blow up the
   ship from their ship and you die."
 }
 
 @the_bridge_text = {
-  "bridge_start" => "You burst onto the Bridge with the netron destruct bomb
+  bridge_start: "You burst onto the Bridge with the netron destruct bomb
   under your arm and surprise 5 Gothons who are trying to
   take control of the ship. Each of them has an even uglier
   clown costume than the last. They haven't pulled their
   weapons out yet, as they see the active bomb under your
   arm and don't want to set it off.",
   
-  "throw_bomb_die" => "In a panic you throw the bomb at the group of Gothons
+  throw_bomb_die: "In a panic you throw the bomb at the group of Gothons
   and make a leap for the door. Right as you drop it a
   Gothon shoots you right in the back killing you.
   As you die you see another Gothon frantically try to disarm
   the bomb. You die knowing they will probably blow up when
   it goes off.",
   
-  "place_bomb_go" => "You point your blaster at the bomb under your arm
+  place_bomb_go: "You point your blaster at the bomb under your arm
   and the Gothons put their hands up and start to sweat.
   You inch backward to the door, open it, and then carefully
   place the bomb on the floor, pointing your blaster at it.
@@ -75,11 +75,11 @@
   Now that the bomb is placed you run to the escape pod to
   get off this tin can.",
   
-  "stuck" => "DOES NOT COMPUTE!"
+  stuck: "DOES NOT COMPUTE!"
 }
 
 @escape_pod_text = {
-  "pod_start" => "You rush through the ship desperately trying to make it to
+  pod_start: "You rush through the ship desperately trying to make it to
   the escape pod before the whole ship explodes. It seems like
   hardly any Gothons are on the ship, so your run is clear of
   interference. You get to the chamber with the escape pods, and
@@ -87,11 +87,11 @@
   but you don't have time to look. There's 5 pods, which one
   do you take?",
 
-  "bad_pod_die" => "The pod escapes out into the void of space, then
+  bad_pod_die: "The pod escapes out into the void of space, then
   implodes as the hull ruptures, crushing your body
   into jam jelly.",
   
-  "good_pod_win" => "The pod easily slides out into space heading to
+  good_pod_win: "The pod easily slides out into space heading to
   the planet below. As it flies to the planet, you look
   back and see your ship implode then explode like a
   bright star, taking out the Gothon ship at the same
