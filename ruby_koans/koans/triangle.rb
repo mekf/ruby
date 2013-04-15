e21d6907686222395a93d74f939b76296d4260cc
Minh Ha <nhat.minh.ha@gmail.com>
2013-04-15 14:03:25 +1200

    finished about_triangle_project_2 with hints from stackoverflow

--- ruby_koans/koans/triangle.rb
+++ ruby_koans/koans/triangle.rb
@@ -14,26 +14,38 @@
 #   about_triangle_project_2.rb
 #
 
+# http://stackoverflow.com/questions/3834203/ruby-koan-151-raising-exceptions
+# MOAR elegant
+
 def triangle(a, b, c)
-  equal_sides = equal_sides_count(a, b, c)
-  if equal_sides == 3
-    :equilateral
-  elsif equal_sides == 2
-    :isosceles
-  else
-    :scalene
-  end
+  x, y, z = [a, b, c].sort
+  
+  raise TriangleError, "invalid length" if x <= 0
+  raise TriangleError, "sum of 2 sides !> 1 side" if x + y <= z
+  
+  [nil, :equilateral, :isosceles, :scalene][[a, b, c].uniq.size]
 end
 
-def equal_sides_count(a, b, c)
-  if a == b and a == c
-    3
-  elsif a != b and a != c and b != c
-    0
-  else
-    2
-  end
-end
+# def triangle(a, b, c)
+#   equal_sides = equal_sides_count(a, b, c)
+#   if equal_sides == 3
+#     :equilateral
+#   elsif equal_sides == 2
+#     :isosceles
+#   else
+#     :scalene
+#   end
+# end
+
+# def equal_sides_count(a, b, c)
+#   if a == b and a == c
+#     3
+#   elsif a != b and a != c and b != c
+#     0
+#   else
+#     2
+#   end
+# end
 
 # only playing around with *args
 # def equal_sides_count(*args)
