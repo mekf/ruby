694e0d0f084e0370c2ee7c8d5000d53ea859e51b
Minh Ha <nhat.minh.ha@gmail.com>
2013-04-15 13:12:41 +1200

    finished the first triangle

--- ruby_koans/koans/triangle.rb
+++ ruby_koans/koans/triangle.rb
@@ -13,8 +13,26 @@
 # and
 #   about_triangle_project_2.rb
 #
+
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  equal_sides = equal_sides_count(a, b, c)
+  if equal_sides == 3
+    :equilateral
+  elsif equal_sides == 2
+    :isosceles
+  else
+    :scalene
+  end
+end
+
+def equal_sides_count(*args)
+  if args[0] == args[1] and args[0] == args[2]
+    3
+  elsif args[0] != args[1] and args[0] != args[2] and args[1] != args[2]
+    0
+  else
+    2
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
