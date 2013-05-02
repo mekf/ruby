20b343619ec55ce1df93dab46da0731aa782f0a8
Minh Ha <nhat.minh.ha@gmail.com>
2013-05-02 18:28:22 +1200

    typo in submits guess feature, working total match count, and number match count

--- rspec_book/codebreaker/spec/codebreaker/marker_spec.rb
+++ rspec_book/codebreaker/spec/codebreaker/marker_spec.rb
@@ -61,7 +61,6 @@ module Codebreaker
 
       context 'with 1 exact match duplicated in guess' do
         it 'returns 0' do
-          pending("refactoring marker")
           marker = Marker.new('1234', '1155')
           marker.number_match_count.should == 0
         end
