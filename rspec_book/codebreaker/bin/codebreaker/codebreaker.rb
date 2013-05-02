24be7c43f0d110e083fcf5eb8cb44bb238375635
Minh Ha <nhat.minh.ha@gmail.com>
2013-05-02 19:21:35 +1200

    generate random secret code at start

--- rspec_book/codebreaker/bin/codebreaker/codebreaker.rb
+++ rspec_book/codebreaker/bin/codebreaker/codebreaker.rb
@@ -2,8 +2,17 @@
 $LOAD_PATH.unshift File.expand_path('../../../lib', __FILE__)
 require 'codebreaker'
 
+def generate_secret_code
+  code = []
+  4.times { code.push(rand(1..9)) }
+  code = code.join
+end
+
 game = Codebreaker::Game.new(STDOUT)
-game.start('1234')
+secret_code = generate_secret_code
+at_exit { puts "secret_code: #{secret_code}" }
+
+game.start(secret_code)
 while guess = gets.chomp
   game.guess(guess)
 end
