f62e1dc3c8871282d31fa6641a1192f2d757f768
Minh Ha <nhat.minh.ha@gmail.com>
2013-05-02 12:05:25 +1200

    no matches, 1 exact, 1 number match

--- rspec_book/codebreaker/lib/codebreaker/game.rb
+++ rspec_book/codebreaker/lib/codebreaker/game.rb
@@ -5,11 +5,28 @@ module Codebreaker
     end
 
     def start(secret)
+      @secret = secret
       @output.puts 'Welcome to Codebreaker!'
       @output.puts 'Enter guess:'
     end
 
     def guess(guess)
+      if exact_match?(guess, 0)      
+        mark = '+'
+      elsif number_match?(guess, 0)
+        mark = '-'
+      else
+        mark = ''
+      end
+      @output.puts mark
+    end
+
+    def exact_match?(guess, index)
+      @secret[index] == guess[index]
+    end
+
+    def number_match?(guess, index)
+      @secret.include?(guess[index])
     end
   end
 end
--- rspec_book/codebreaker/spec/codebreaker/game_spec.rb
+++ rspec_book/codebreaker/spec/codebreaker/game_spec.rb
@@ -2,11 +2,11 @@ require 'spec_helper'
 
 module Codebreaker 
   describe Game do
-    describe "#start" do
-      # as_null_object described in pg.71
-      let(:output) { double('output').as_null_object }
-      let(:game) { Game.new(output) }
+    # as_null_object described in pg.71
+    let(:output) { double('output').as_null_object }
+    let(:game) { Game.new(output) }
 
+    describe "#start" do
       it "sends a welcome message" do
         output.should_receive(:puts).with('Welcome to Codebreaker!')
         game.start('1234')
@@ -16,6 +16,30 @@ module Codebreaker
         output.should_receive(:puts).with('Enter guess:')
         game.start('1234')
       end
-    end 
+    end
+
+    describe '#guess' do
+      context 'with no matches' do
+        it 'sends a mark with ""' do
+          game.start('1234')
+          output.should_receive(:puts).with('')
+          game.guess('5555')
+        end
+      end
+      context 'with 1 number match' do
+        it 'sends a mark with "-"' do
+          game.start('1234')
+          output.should_receive(:puts).with('-')
+          game.guess('2555')
+        end
+      end
+      context 'with 1 exact match' do
+        it 'sends a mark with "+"' do
+          game.start('1234')
+          output.should_receive(:puts).with('+')
+          game.guess('1555')
+        end
+      end
+    end    
   end
 end
