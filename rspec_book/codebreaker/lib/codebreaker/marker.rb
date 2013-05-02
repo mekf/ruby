25c3999533cf8a2c277d9da15c364d86e88eaf64
Minh Ha <nhat.minh.ha@gmail.com>
2013-05-02 21:08:25 +1200

    stripped no longer used code, changing order for readability

--- rspec_book/codebreaker/lib/codebreaker/marker.rb
+++ rspec_book/codebreaker/lib/codebreaker/marker.rb
@@ -3,27 +3,6 @@ class Marker
     @secret, @guess = secret, guess
   end
 
-  def exact_match?(index)
-    @secret[index] == @guess[index]
-  end
-
-  def number_match?(index)
-    @secret.include?(@guess[index]) and !exact_match?(index)
-  end
-
-  # inject(0) -> initialize 'count' with 0
-  # run through 'index', return new 'count'
-  # assign 'count' as the start of the next iteration
-  def exact_match_count
-    (0..3).inject(0) do |count, index|
-      count + (exact_match?(index) ? 1 : 0)
-    end
-  end
-
-  def number_match_count
-    total_match_count - exact_match_count
-  end
-
   def total_match_count
     secret = @secret.split('')
     @guess.split('').inject(0) do |count, n|
@@ -35,5 +14,22 @@ class Marker
   # returns nil if no match is found
   def delete_from_secret(code, n)
     code.delete_at(code.index(n)) if code.index(n)
+  end
+
+  # inject(0) -> initialize 'count' with 0
+  # run through 'index', return new 'count'
+  # assign 'count' as the start of the next iteration
+  def exact_match_count
+    (0..3).inject(0) do |count, index|
+      count + (exact_match?(index) ? 1 : 0)
+    end
+  end  
+
+  def exact_match?(index)
+    @secret[index] == @guess[index]
+  end
+
+  def number_match_count
+    total_match_count - exact_match_count
   end
 end
