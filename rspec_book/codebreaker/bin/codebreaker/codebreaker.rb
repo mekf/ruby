31d4a382558fdba773000659e14cc266e98bccd5
Minh Ha <nhat.minh.ha@gmail.com>
2013-05-02 19:58:05 +1200

    refactor generate_secret_code to not use temp variable

--- rspec_book/codebreaker/bin/codebreaker/codebreaker.rb
+++ rspec_book/codebreaker/bin/codebreaker/codebreaker.rb
@@ -3,16 +3,15 @@ $LOAD_PATH.unshift File.expand_path('../../../lib', __FILE__)
 require 'codebreaker'
 
 def generate_secret_code
-  code = []
-  4.times { code.push(rand(1..9)) }
-  code = code.join
+  options = (1..9).to_a
+  (0..3).map { options.delete_at(rand(options.length)) }.join
 end
 
-game = Codebreaker::Game.new(STDOUT)
 secret_code = generate_secret_code
+game = Codebreaker::Game.new(STDOUT)
+game.start(secret_code)
 at_exit { puts "secret_code: #{secret_code}" }
 
-game.start(secret_code)
 while guess = gets.chomp
   game.guess(guess)
 end
