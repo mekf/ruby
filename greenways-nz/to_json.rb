be7e31a505855a8b3f3d763d7abed903b637a067
Minh Ha <nhat.minh.ha@gmail.com>
2013-05-16 01:20:55 +1200

    kinda working script

--- greenways-nz/to_json.rb
+++ greenways-nz/to_json.rb
@@ -1,10 +1,28 @@
 # converting a list of locations to json
 
-source      = File.open('locations')
-destination = File.open('locations.json', 'w')
+# Bhana Brothers Fruiterers
+# 129 Ponsonby Road, Ponsonby
+# Auckland
+# 09 3765329
+
+source = File.open('locations')
+target = File.open('locations.json', 'w')
 
 lines = source.readlines
 
 # for i in 0...lines.length
 #   puts "#{i} :" + lines[i]
 # end
+
+1.times do
+  i = 0
+  target.write("{")
+  target.write("\n\t" + '"name":'    + "\s\"#{lines[i].strip}\",")
+  target.write("\n\t" + '"address":' + "\s\"#{lines[i + 1].strip}\",")
+  target.write("\n\t" + '"city":'    + "\s\"#{lines[i + 2].strip}\",")
+  target.write("\n\t" + '"phone":'   + "\s\"#{lines[i + 3].strip}\"")
+  target.write("\n},")
+end
+
+source.close
+target.close
