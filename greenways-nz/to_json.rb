e26cb4c995128b5dfa3930671621393ce7177c32
Minh Ha <nhat.minh.ha@gmail.com>
2013-05-16 03:34:52 +1200

    valid json

--- greenways-nz/to_json.rb
+++ greenways-nz/to_json.rb
@@ -20,25 +20,37 @@ target_file = File.open('locations.json', 'w')
 @lines  = source_file.readlines
 
 def write_line(num)
-  @target.write("\n\t" + '"name":'    + "\s\"#{@lines[num].strip}\",")
-  @target.write("\n\t" + '"address":' + "\s\"#{@lines[num + 1].strip}\",")
-  @target.write("\n\t" + '"region":'  + "\s\"#{@lines[num + 2].strip}\",")
-  @target.write("\n\t" + '"phone":'   + "\s\"#{@lines[num + 3].strip}\"")
+  @target.write("\n\t\t" + '"name":'    + "\s\"#{@lines[num].strip}\",")
+  @target.write("\n\t\t" + '"address":' + "\s\"#{@lines[num + 1].strip}\",")
+  @target.write("\n\t\t" + '"region":'  + "\s\"#{@lines[num + 2].strip}\",")
+  @target.write("\n\t\t" + '"phone":'   + "\s\"#{@lines[num + 3].strip}\"")
 end
 
-i = 0
-while i < @lines.length
-  @target.write("{")
-  write_line(i)
-  
-  if i < @lines.length - 4
-    @target.write("\n},\n")
-  else
-    @target.write("\n}")
+def write_entry
+  i = 0
+  while i < @lines.length
+    @target.write("\t{")
+    write_line(i)
+    
+    if i < @lines.length - 4
+      @target.write("\n\t},\n")
+    else
+      @target.write("\n\t}")
+    end
+    
+    i += 4
   end
-  
-  i += 4
 end
 
+def write_outline
+  @target.write("{")
+  @target.write("\"locations\":\s")
+  @target.write("[\n")
+  write_entry
+  @target.write("\n]")
+  @target.write("}")
+end
+
+write_outline
 source_file.close
 target_file.close
