091eeb3c4f6a66038d40f1e363b9865a206c05cd
Minh Ha <nhat.minh.ha@gmail.com>
2013-06-14 18:33:55 +1200

    little better code

--- nokogiri-test/nokogiri.rb
+++ nokogiri-test/nokogiri.rb
@@ -10,13 +10,14 @@ page_2 = Nokogiri::HTML(RestClient.get(page_url_2))
 page_3 = Nokogiri::HTML(RestClient.get(page_url_3))
 
 def get_title(page)
-  page.xpath("//meta[@property='og:title']")[0]['content']
+  og_title_meta = page.xpath("//meta[@property='og:title']")
+  og_title_meta[0]['content']
 end
 
 def get_description(page)
-  contain_og_description = page.xpath("//meta[@property='og:description']")
-  if contain_og_description.size != 0
-    page.xpath("//meta[@property='og:description']")[0]['content']
+  og_description_meta = page.xpath("//meta[@property='og:description']")
+  if og_description_meta.size != 0
+    og_description_meta[0]['content']
   else
     page.xpath("//meta[@name='description' or @name='Description']")[0]['content']
   end
