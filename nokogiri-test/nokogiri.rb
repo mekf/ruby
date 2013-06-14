c200a3d9e9441e37c8d1b18ee986ea6ecf49011d
Minh Ha <nhat.minh.ha@gmail.com>
2013-06-15 02:30:43 +1200

    refactoring

--- nokogiri-test/nokogiri.rb
+++ nokogiri-test/nokogiri.rb
@@ -10,45 +10,34 @@ def valid_url?(url)
   end
 end
 
+def fetch_article_details(url)
+  page = Nokogiri::HTML(RestClient.get(url)) if valid_url?(url)
+  details = {
+    title:        get_title(page),
+    description:  get_description(page)
+  } if page
+end
+
+def get_title(page)
+  og_title_meta = page.xpath("//meta[@property='og:title']")
+  og_title_meta[0]['content']
+end
+
+def get_description(page)
+  og_description_meta = page.xpath("//meta[@property='og:description']")
+  if og_description_meta.size != 0
+    og_description_meta[0]['content']
+  else
+    page.xpath("//meta[@name='description' or @name='Description']")[0]['content']
+  end
+end
+
 page_url_1 = 'http://www.economist.com/blogs/democracyinamerica/2013/06/surveillance-0'
 page_url_2 = 'http://www.bbc.co.uk/news/world-us-canada-22883078'
 page_url_3 = 'http://www.reuters.com/article/2013/06/12/us-google-nsa-transparency-request-idUSBRE95A11820130612'
 page_url_4 = 'safg.sfwofjowag.fdsafasdf.fdsafads'
 
-page_1 = Nokogiri::HTML(RestClient.get(page_url_1))
-page_2 = Nokogiri::HTML(RestClient.get(page_url_2))
-page_3 = Nokogiri::HTML(RestClient.get(page_url_3))
-page_4 = Nokogiri::HTML(RestClient.get(page_url_4)) if valid_url?(page_url_4)
-
-def get_title(page)
-  unless page == nil
-    og_title_meta = page.xpath("//meta[@property='og:title']")
-    og_title_meta[0]['content']
-  end
-end
-
-def get_description(page)
-  unless page == nil
-    og_description_meta = page.xpath("//meta[@property='og:description']")
-    if og_description_meta.size != 0
-      og_description_meta[0]['content']
-    else
-      page.xpath("//meta[@name='description' or @name='Description']")[0]['content']
-    end
-  end
-end
-
-p get_title(page_1)
-p get_description(page_1)
-
-p ""
-p get_title(page_2)
-p get_description(page_2)
-
-p ""
-p get_title(page_3)
-p get_description(page_3)
-
-p ""
-p get_title(page_4)
-p get_description(page_4)
+p fetch_article_details(page_url_1)
+p fetch_article_details(page_url_2)
+p fetch_article_details(page_url_3)
+p fetch_article_details(page_url_4)
