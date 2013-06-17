825fd4efef73a52a209ae4711dce2f451d57fe37
Minh Ha <nhat.minh.ha@gmail.com>
2013-06-18 11:55:07 +1200

    try using a stub

--- pismo/pismo.rb
+++ pismo/pismo.rb
@@ -1,18 +1,25 @@
 require 'pismo'
 
-page = Pismo::Document.new('http://www.bbc.co.uk/news/world-us-canada-22945003')
+doc_1 = Pismo::Document.new('http://www.bbc.co.uk/news/business-22931899')
+doc_2 = Pismo::Document.new(File.open('stub/business-22931899.html'))
 
-p 'title: '     + page.title
-p 'desc: '      + page.description
-p 'lede: '      + page.lede
-p 'body: '      + page.body
-p 'html_body: ' + page.html_body
-p 'sentences: ' + page.sentences(3)
-# p 'keywords: '  + page.keywords
+def parse_detaiils(page)
+  p 'title: '     + page.title
+  p 'desc: '      + page.description
+  p 'lede: '      + page.lede
+  p 'body: '      + page.body
+  p 'html_body: ' + page.html_body
+  p 'sentences: ' + page.sentences(3)
+  # p 'keywords: '  + page.keywords
 
-# Stuffs that are likely not available
-# p 'author: '    + page.author if page.author
-# p 'authors: '   + page.authors
+  # Stuffs that are likely not available
+  # p 'author: '    + page.author if page.author
+  # p 'authors: '   + page.authors
 
-# p 'feed: '      + page.feed
-# p 'feeds: '     + page.feeds
+  # p 'feed: '      + page.feed
+  # p 'feeds: '     + page.feeds
+end
+
+parse_detaiils(doc_1)
+3.times {  p '#'  }
+parse_detaiils(doc_2)
