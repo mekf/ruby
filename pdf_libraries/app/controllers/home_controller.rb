e9c9eef9f5a32c7d60c5da82a415ed26cd30a5fe
Minh Ha <nhat.minh.ha@gmail.com>
2013-06-27 00:52:01 +1200

    basic pdf up n' running'

--- pdf_libraries/app/controllers/home_controller.rb
+++ pdf_libraries/app/controllers/home_controller.rb
@@ -3,17 +3,34 @@ class HomeController < ApplicationController
   def index
   end
 
+  def show_simple
+    render layout: 'simple'
+  end
+
   def show
     respond_to do |format|
       format.html
       format.pdf do
-        render pdf: 'file_name',
-          template: 'home/show',
-          format: [:pdf],
-          handlers: [:haml],
-          page_size: 'A4',
-          use_xserver: true
+        render_pdf  
       end
     end
   end
+
+  def render_pdf
+    render pdf: 'file_name',
+      template: 'home/show',
+      format: [:pdf],
+      handlers: [:haml],
+      page_size: 'A4',
+      margin: {
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0
+      }
+      # disable_smart_shrinking: true
+      
+      # use_xserver: true
+  end
+
 end
--- pdf_libraries/config/routes.rb
+++ pdf_libraries/config/routes.rb
@@ -1,4 +1,5 @@
 PdfLibraries::Application.routes.draw do
   root to: 'home#index'
   match 'show', to: 'home#show'
+  match 'show_simple', to: 'home#show_simple'
 end
