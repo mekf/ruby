0d96ccf1c346a4399a69e37b4ba25b750cbbbb37
Minh Ha <nhat.minh.ha@gmail.com>
2013-06-26 15:27:25 +1200

    starting a new pdf_libraries

---
+++ pdf_libraries/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PdfLibraries::Application.config.secret_token = 'e757fd8b6478a33166104c4fc5904080812a6a0f505448266cf1652fed4b40195a8497b15d87aaaf996fbd37d67cbd531bca6adf68c0ed8ede7aedd9b5a17a31'
---
+++ pdf_libraries/config/routes.rb
@@ -0,0 +1,2 @@
+PdfLibraries::Application.routes.draw do
+end
